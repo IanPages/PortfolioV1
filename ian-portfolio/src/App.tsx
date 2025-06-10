706f33fe76d3ab13c1e88bddf953482ccb46abce
IanPages <ianpagesrodriguez@gmail.com>
2025-06-10 13:33:54 +0100

    FEATURE: Added i18n for translation, implemented carrousel for technologies and finished header

--- ian-portfolio/src/App.tsx
+++ ian-portfolio/src/App.tsx
@@ -1,3 +1,4 @@
+import './i18n';
 import { motion } from 'framer-motion';
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
@@ -9,89 +10,89 @@ import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import AnimatedBg from './components/AnimatedBg';
 import { Link as ScrollLink } from 'react-scroll';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
 
   const tech = [
     { name: 'React', img: '/icons8-react-160.png' },
-    {name: 'TypeScript', img: '/icons8-typescript-144.png' },
+    { name: 'TypeScript', img: '/icons8-typescript-144.png' },
     { name: 'Tailwind CSS', img: '/icons8-viento-de-cola-144.png' },
     { name: 'Spring Boot', img: '/icons8-spring-boot-144.png' },
-    { name: 'Java', img: '/icons8-java-144.png'},
-    { name: 'Asp.NET', img: '/net.png'},
-    { name: 'C#', img: '/cSharp.png'},
-    { name: 'Figma', img: '/icons8-figma-144.png'},
-    { name: 'Git', img:'/icons8-git-144.png'},
-    { name: 'Stripe', img: '/icons8-raya-160.png'}
-  ]
+    { name: 'Java', img: '/icons8-java-144.png' },
+    { name: 'Asp.NET', img: '/net.png' },
+    { name: 'C#', img: '/cSharp.png' },
+    { name: 'Figma', img: '/icons8-figma-144.png' },
+    { name: 'Git', img: '/icons8-git-144.png' },
+    { name: 'Stripe', img: '/icons8-raya-160.png' }
+  ];
 
   const experience = [
     {
-      title: 'Internship',
+      title: t('exp1_title'),
       company: 'Eviden',
       date: '02-2025 / 05-2025',
-      description: ' Desarrollo de aplicación completamente funcional para un cine con gestión de salas y películas con gestión de futuras emisiones con Spring Boot, React.js, Stripe y Tailwind CSS.'
-
+      description: t('exp1_desc')
     },
     {
-      title: 'Internship',
+      title: t('exp2_title'),
       company: 'Eviden',
       date: '03-2024 / 05-2024',
-      description: 'Participación en bootcamp sobre Angular, Next.js y SQL. Desarrollo de aplicación completamente funcional y escalable sobre un concesionario de vehículos de segunda mano con pasarela de pago con Angular, Next.js y Paypal.'
+      description: t('exp2_desc')
     },
     {
-      title: 'Internship',
+      title: t('exp3_title'),
       company: 'Partner Ubezpieczenia',
       date: '03-2023 / 06-2023',
-      description: 'Desarrollo de proyecto sobre Linux Server, realizando el host de su aplicación para un total de 30 usuarios con gestión sobre NextCloud. Mantenimiento de material y dispositivos de la empresa.'
+      description: t('exp3_desc')
     }
-  ]
+  ];
 
   const education = [
     {
-      title: 'CFGS Desarrollo de Aplicaciones Web',
+      title: t('edu1_title'),
       college: 'CIFP César Manrique',
       date: '09-2023 / 06-2025',
     },
     {
-      title: 'English B1 Certificate',
+      title: t('edu2_title'),
       college: 'That´s English',
       date: '09-2022 / 06-2023',
     },
     {
-      title: 'CFGM Sistemas Microinformáticos y Redes',
+      title: t('edu3_title'),
       college: 'CIFP César Manrique',
       date: '09-2021 / 06-2023',
     }
-  ]
+  ];
 
   const projects = [
     {
       inprogress: false,
-      name: 'Cinema App',
-      description: 'Proyecto grupal de cine con gestión de salas y películas con futuras emisiones, desarrollado con Spring Boot, React.js, Tailwind CSS, Stripe y Cloudinary.',
+      name: t('proj1_name'),
+      description: t('proj1_desc'),
       img: '/cinema-app.png',
       link1: 'https://github.com/emmanuelbg-10/Cine-Back.git',
       link2: 'https://github.com/emmanuelbg-10/Cine-Front.git'
     },
     {
       inprogress: false,
-      name: 'Ticketflow',
-      description: 'Proyecto de fin de grado sobre venta de tickets para eventos indoor y outdoor, desarrollado con Spring Boot, Angular, Tailwind CSS, Stripe y Cloudinary.',
+      name: t('proj2_name'),
+      description: t('proj2_desc'),
       img: '/ticketflow.png',
       link1: 'https://github.com/IanPages/TicketFlowBack.git',
       link2: 'https://github.com/IanPages/TicketFlowFront.git'
     },
     {
       inprogress: true,
-      name: 'SkillHub | In Progress',
-      description: 'Aplicación para freelancers y empresas para encontrar a profesionales y proyectos nuevos, desarro sobre Spring Boot, React.js, Tailwind CSS, Stripe y Cloudinary.',
+      name: t('proj3_name'),
+      description: t('proj3_desc'),
       img: '/skillhub.png',
       link1: '',
       link2: ''
     }
-    
-  ]
+  ];
 
   const [formErrors, setFormErrors] = React.useState<{ name?: string; email?: string; message?: string }>({});
   const formRef = React.useRef<HTMLFormElement>(null);
@@ -102,10 +103,10 @@ function App() {
     const emailInput = form.elements.namedItem('email') as HTMLInputElement | null;
     const messageInput = form.elements.namedItem('message') as HTMLTextAreaElement | null;
 
-    if (!nameInput || !nameInput.value.trim()) errors.name = "El nombre es obligatorio";
-    if (!emailInput || !emailInput.value.trim()) errors.email = "El email es obligatorio";
-    else if (!/\S+@\S+\.\S+/.test(emailInput.value)) errors.email = "Email no válido";
-    if (!messageInput || !messageInput.value.trim()) errors.message = "El mensaje es obligatorio";
+    if (!nameInput || !nameInput.value.trim()) errors.name = t('form_error_name');
+    if (!emailInput || !emailInput.value.trim()) errors.email = t('form_error_email');
+    else if (!/\S+@\S+\.\S+/.test(emailInput.value)) errors.email = t('form_error_email_invalid');
+    if (!messageInput || !messageInput.value.trim()) errors.message = t('form_error_message');
     return errors;
   };
 
@@ -119,11 +120,11 @@ function App() {
 
     emailjs.sendForm('service_wl4lafq', 'template_4p075a5', form, 'dXQ1ADco9fFVpqLwz')
       .then(() => {
-        toast.success('¡Correo enviado satisfactoriamente!');
+        toast.success(t('form_success'));
         setFormErrors({});
         form.reset();
       }, () => {
-        toast.error('Error enviando el correo. Inténtalo de nuevo.');
+        toast.error(t('form_error'));
       });
   };
 
@@ -131,57 +132,56 @@ function App() {
     <div className=" text-white min-h-screen scroll-smooth flex flex-col ">
       <AnimatedBg />
       <Header />
-      <motion.div
-        id="about"
-        className='py-12'
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-      >
+      <motion.div id="about" className='py-12' initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
         <div className='bg-gradient-to-br from-gray-700 to-gray-900 rounded  flex flex-col items-center justify-center text-center py-12 space-y-4 max-w-4xl mx-auto'>
           <img src="/ian.jpg" alt="Ian Pagés Rodríguez" className='w-32 h-32 rounded-full shadow-lg mb-4' />
           <h1>Ian Pagés Rodríguez</h1>
-          <p>FullStack developer apasionado y curioso por las nuevas tecnologías</p>
+          <p>{t('about_me')}</p>
           <button className='bg-red-500 hover:bg-red-800 duration-500 p-2 rounded shadow-md font-semibold'>
-        <ScrollLink to="contact" smooth={true} duration={600} offset={-60} className="block px-4 py-2 cursor-pointer">Contactar</ScrollLink>
+            <ScrollLink to="contact" smooth={true} duration={600} offset={-60} className="block px-4 py-2 cursor-pointer">{t('contact_button')}</ScrollLink>
           </button>
         </div>
       </motion.div>
-      <motion.div id="technologies" className='py-12' initial={{ opacity: 0, y: 40}}
-      whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount:0.3 }}>
+      <motion.div
+        id="technologies"
+        className="py-12"
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true, amount: 0.01 }}
+      >
         <div className="flex items-center justify-center mb-8">
-         
           <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-8 sm:mb-12 text-center flex items-center justify-center gap-2 sm:gap-3">
             <FaTools className="text-3xl sm:text-4xl text-yellow-400 mr-1 sm:mr-2" />
-            Tecnologías y Herramientas
+            {t('technologies_tools')}
           </h2>
         </div>
-        <div className='bg-gradient-to-br from-gray-700  to-gray-900 rounded-xl shadow-lg p-8 max-w-4xl mx-auto'>
-          <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8'>
-          {tech.map((item) => {
-            return (
-              <div key={item.name} className='relative group flex flex-col items-center'>
-                <img src={item.img} alt={item.name} className='w-20 h-20 object-contain transition-transform duration-300 group-hover:scale-110 drop-shadow-lg' />
-                <span className='absolute -top-6 left-1/2 -translate-x-1/2 bg-black bg-opacity-80 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-10'>
-                  {item.name}
-                </span>
-              </div>
-              
-            )
-          })}  
+        <div className='bg-gradient-to-br from-gray-700 to-gray-900 rounded-xl shadow-lg p-8 max-w-4xl mx-auto'>
+          <div className="overflow-hidden relative group h-auto" >
+            <motion.div
+              className="flex gap-8 w-max"
+              style={{ willChange: 'transform' }}
+              animate={{ x: ['0%', '-50%'] }}
+              transition={{ repeat: Infinity, repeatType: 'loop', duration: 12, ease: 'linear', }}>
+              {[...tech, ...tech].map((item, idx) => (
+                <div key={item.name + idx} className='relative group flex flex-col items-center cursor-pointer'>
+                  <img src={item.img} alt={item.name}
+                    className='w-12 h-12 lg:h-20 lg:w-20 shadow-lg' /></div>
+              ))}
+            </motion.div>
           </div>
         </div>
       </motion.div>
-      <motion.div id="experience" className='py-12' initial={{ opacity: 0, y: 40}}
-      whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount:0.3  }}>
+      <motion.div id="experience" className='py-12' initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount: 0.3 }}>
         <h2 className='text-3xl font-bold mb-12 text-center flex items-center justify-center gap-3'>
           <FaGraduationCap className="text-4xl text-yellow-400 mr-2" />
-          Experiencia y Estudios</h2>
+          {t('experience_education')}
+        </h2>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-4xl mx-auto'>
           <div>
-            <h3 className='text-xl font-semibold mb-6 ml-6'>Experiencia Laboral</h3>
+            <h3 className='text-xl font-semibold mb-6 ml-6'>{t('work_experience')}</h3>
             <ul className='space-y-6'>
-              {experience.map((exp,id) => {
+              {experience.map((exp, id) => {
                 return (
                   <li key={id} className='border-l-4 border-red-500 pl-4'>
                     <h4 className='font-bold'>{exp.title}</h4>
@@ -193,9 +193,9 @@ function App() {
             </ul>
           </div>
           <div>
-            <h3 className='text-xl font-semibold mb-6 ml-6'>Educación</h3>
+            <h3 className='text-xl font-semibold mb-6 ml-6'>{t('education')}</h3>
             <ul className='space-y-6'>
-              {education.map((edu,id) => {
+              {education.map((edu, id) => {
                 return (
                   <li key={id} className='border-l-4 border-blue-500 pl-4'>
                     <h4 className='font-bold'>{edu.title}</h4>
@@ -206,15 +206,15 @@ function App() {
             </ul>
           </div>
         </div>
-        
       </motion.div>
-      <motion.div id="projects" className='py-12' initial={{ opacity: 0, y: 40}}
-      whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount:0.3  }}>
+      <motion.div id="projects" className='py-12' initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount: 0.3 }}>
         <h2 className='text-3xl font-bold mb-12 text-center flex items-center justify-center gap-3'>
           <FaProjectDiagram className="text-4xl text-yellow-400 mr-2" />
-          Proyectos</h2>
+          {t('projects')}
+        </h2>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-4xl mx-auto'>
-          {projects.map((project,id) => {
+          {projects.map((project, id) => {
             return (
               <div key={id} className={`bg-gray-800 rounded-lg shadow-lg overflow-hidden ${project.inprogress ? 'opacity-50' : ''}`}>
                 <img src={project.img} alt={project.name} className='' />
@@ -223,9 +223,9 @@ function App() {
                   <p className='text-gray-300 mb-4'>{project.description}</p>
                   <div className='flex space-x-4'>
                     {project.link1 && (
-                      <a href={project.link1} target="_blank" rel="noopener noreferrer" className='text-yellow-300 hover:underline'> Código Back </a> )}
+                      <a href={project.link1} target="_blank" rel="noopener noreferrer" className='text-yellow-300 hover:underline'>{t('code_back')}</a>)}
                     {project.link2 && (
-                      <a href={project.link2} target="_blank" rel="noopener noreferrer" className='text-yellow-500 hover:underline'> Código Front </a>)}
+                      <a href={project.link2} target="_blank" rel="noopener noreferrer" className='text-yellow-500 hover:underline'>{t('code_front')}</a>)}
                   </div>
                 </div>
               </div>
@@ -233,31 +233,31 @@ function App() {
           })}
         </div>
       </motion.div>
-      
-      <motion.div id="contact" className='py-12' initial={{ opacity: 0, y: 40}}
-        whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true , amount:0.3 }}>
+
+      <motion.div id="contact" className='py-12' initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount: 0.3 }}>
         <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-8 sm:mb-12 text-center flex items-center justify-center gap-2 sm:gap-3">
           <FaEnvelopeOpenText className="text-3xl sm:text-4xl text-yellow-400 mr-1 sm:mr-2" />
-          Contacto
+          {t('contact')}
         </h2>
         <form ref={formRef} className='flex flex-col space-y-4 max-w-xl mx-auto bg-gray-800 p-4 rounded' onSubmit={sendEmail} noValidate>
-          <label>Nombre</label>
+          <label>{t('form_name')}</label>
           <input type="text" name="name" className='p-2 rounded bg-gray-700 text-white' />
           {formErrors.name && <span className="text-red-400 text-sm">{formErrors.name}</span>}
 
-          <label>Email</label>
+          <label>{t('form_email')}</label>
           <input type="email" name="email" className='p-2 rounded bg-gray-700 text-white' />
           {formErrors.email && <span className="text-red-400 text-sm">{formErrors.email}</span>}
 
-          <label>Mensaje</label>
+          <label>{t('form_message')}</label>
           <textarea name="message" className='p-2 rounded text-white bg-gray-700 h-32'></textarea>
           {formErrors.message && <span className="text-red-400 text-sm">{formErrors.message}</span>}
 
-          <button type="submit" className='bg-red-500 hover:bg-red-800 duration-500 p-2 cursor-pointer rounded shadow-md font-semibold'>Enviar</button>
+          <button type="submit" className='bg-red-500 hover:bg-red-800 duration-500 p-2 cursor-pointer rounded shadow-md font-semibold'>{t('form_send')}</button>
         </form>
         <ToastContainer position="top-center" autoClose={3000} hideProgressBar />
       </motion.div>
-      
+
       <Footer />
     </div>
   )
--- ian-portfolio/src/components/Header.tsx
+++ ian-portfolio/src/components/Header.tsx
@@ -1,35 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link as ScrollLink } from "react-scroll";
+import { useTranslation } from "react-i18next";
 
 export const Header = () => {
     const [open, setOpen] = useState(false);
+    const { t, i18n } = useTranslation();
+
+    const isSpanish = i18n.language === "es" || !["en", "es"].includes(i18n.language);
+
+    const handleLanguageToggle = () => {
+        i18n.changeLanguage(isSpanish ? "en" : "es");
+    };
+
+    useEffect(() => {
+        if (open) {
+            document.body.classList.add("overflow-hidden");
+        } else {
+            document.body.classList.remove("overflow-hidden");
+        }
+        // Limpieza por si el componente se desmonta con el menú abierto
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, [open]);
 
     return (
-        <header className="flex items-center justify-between lg:justify-around p-4  text-white">
+        <header className="flex items-center justify-between md:justify-around p-4  text-white">
         <h1 className="text-2xl font-bold cursor-pointer"><a href="">&lt;IanPagés /&gt;</a></h1>
-        
         <nav className="hidden lg:block">
-    <ul className="flex space-x-4 bg-black p-2 rounded-lg px-4">
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline">
-            // <ScrollLink to="about" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">Sobre Mí</ScrollLink>
+    <ul className="flex space-x-4 items-center justify-center  p-2 rounded-lg px-4">
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline"> <ScrollLink to="about" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">{t("about")}</ScrollLink></li>
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline"> <ScrollLink to="technologies" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">{t("technologies")}</ScrollLink></li>
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline"> <ScrollLink to="experience" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">{t("experience")}</ScrollLink></li>
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline"> <ScrollLink to="projects" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">{t("projects")}</ScrollLink></li>
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline"> <ScrollLink to="contact" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">{t("contact")}</ScrollLink></li>
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline"> <a href="/CVIanPages.pdf" download target="_blank" rel="noopener noreferrer" className="ml-2 cursor-pointer">{t("download_cv")}</a></li>
+        <li className=" p-2 rounded-lg hover:scale-110 duration-400 hover:underline cursor-pointer flex items-center" onClick={handleLanguageToggle}>
+            <img src={isSpanish ? "/es-flag.png" : "/uk-flag.png"} alt="flag" className="w-12 h-8 inline" />
         </li>
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline">
-            // <ScrollLink to="technologies" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">Tecnologías</ScrollLink>
-        </li>
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline">
-            // <ScrollLink to="experience" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">Experiencia</ScrollLink>
-        </li>
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline">
-            // <ScrollLink to="projects" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">Proyectos</ScrollLink>
-        </li>
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline">
-            // <ScrollLink to="contact" smooth={true} duration={600} offset={-60} className="ml-2 cursor-pointer">Contacto</ScrollLink>
-        </li>
-        {/*Añadir la descarga del CV */}
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline">Descargar CV</li>
-        <li className="bg-gray-800 p-2 rounded-lg hover:scale-110 duration-400 hover:underline cursor-pointer">Idioma</li>
     </ul>
 </nav>
         
@@ -42,36 +52,16 @@ export const Header = () => {
             <AnimatePresence>
     {open && (
         <motion.ul
-            initial={{ opacity: 0, y: -10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.2 }}
-            className="fixed left-0 w-full mt-4 bg-gray-900 bg-opacity-50 flex flex-col items-center text-center justify-center z-50"
-        >
-            <li>
-                <ScrollLink to="about" smooth={true} duration={600} offset={-60} className="block px-4 py-2 hover:bg-gray-700" onClick={() => setOpen(false)}>
-                    Sobre Mí
-                </ScrollLink>
-            </li>
-            <li>
-                <ScrollLink to="technologies" smooth={true} duration={600} offset={-60} className="block px-4 py-2 hover:bg-gray-700" onClick={() => setOpen(false)}>
-                    Tecnologías
-                </ScrollLink>
-            </li>
-            <li>
-                <ScrollLink to="experience" smooth={true} duration={600} offset={-60} className="block px-4 py-2 hover:bg-gray-700" onClick={() => setOpen(false)}>
-                    Experiencia
-                </ScrollLink>
-            </li>
-            <li>
-                <ScrollLink to="projects" smooth={true} duration={600} offset={-60} className="block px-4 py-2 hover:bg-gray-700" onClick={() => setOpen(false)}>
-                    Proyectos
-                </ScrollLink>
-            </li>
-            <li>
-                <ScrollLink to="contact" smooth={true} duration={600} offset={-60} className="block px-4 py-2 hover:bg-gray-700" onClick={() => setOpen(false)}>
-                    Contacto
-                </ScrollLink>
+            initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.2 }}
+            className="fixed left-0 w-full mt-4 bg-gray-700 bg-opacity-50 flex flex-col items-center text-center justify-center z-50 p-4 rounded-lg shadow-lg">
+            <li><ScrollLink to="about" smooth={true} duration={600} offset={-60} className="block px-4 py-4 cursor-pointer duration-300 hover:underline hover:scale-110" onClick={() => setOpen(false)}>{t("about")}</ScrollLink></li>
+            <li><ScrollLink to="technologies" smooth={true} duration={600} offset={-60} className="block px-4 py-4 cursor-pointer duration-300 hover:underline hover:scale-110" onClick={() => setOpen(false)}>{t("technologies")}</ScrollLink></li>
+            <li><ScrollLink to="experience" smooth={true} duration={600} offset={-60} className="block px-4 py-4 cursor-pointer duration-300 hover:underline hover:scale-110" onClick={() => setOpen(false)}>{t("experience")}</ScrollLink></li>
+            <li><ScrollLink to="projects" smooth={true} duration={600} offset={-60} className="block px-4 py-4 cursor-pointer duration-300 hover:underline hover:scale-110" onClick={() => setOpen(false)}>{t("projects")}</ScrollLink></li>
+            <li><ScrollLink to="contact" smooth={true} duration={600} offset={-60} className="block px-4 py-4 cursor-pointer duration-300 hover:underline hover:scale-110" onClick={() => setOpen(false)}>{t("contact")}</ScrollLink></li>
+            <li><a href="/CVIanPages.pdf" download target="_blank" rel="noopener noreferrer" className="block px-4 py-4 cursor-pointer duration-300 hover:underline hover:scale-110" onClick={() => setOpen(false)}>{t("download_cv")}</a></li>
+            <li className="cursor-pointer px-4 py-2 duration-300 hover:underline hover:scale-110 flex items-center" onClick={() => { handleLanguageToggle(); setOpen(false); }}>
+                <img src={isSpanish ? "/es-flag.png" : "/uk-flag.png"} alt="flag" className="w-12 h-8 inline" />
             </li>
         </motion.ul>
     )}
